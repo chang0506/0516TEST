8ccfa9ed25e3b585e46110d8309d731c4f9ece28
chang0506 <j8970@naver.com>
2025-05-16 09:42:43 +0900

    create pj

---
+++ korea_sleepTech_test_0516/src/main/java/com/example/korea_sleepTech_test_0516/KoreaSleepTechTest0516Application.java
@@ -0,0 +1,13 @@
+package com.example.korea_sleepTech_test_0516;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaSleepTechTest0516Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaSleepTechTest0516Application.class, args);
+	}
+
+}
---
+++ korea_sleepTech_test_0516/src/test/java/com/example/korea_sleepTech_test_0516/KoreaSleepTechTest0516ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.korea_sleepTech_test_0516;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaSleepTechTest0516ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
